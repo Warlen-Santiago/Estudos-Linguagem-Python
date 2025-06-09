337845a9032ec763d1924b52eb09da726033690d
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-06-09 18:38:28 -0300

    Teste 01 em Python

---
+++ Curso Python - Curso em vídeo(Guanabara)/Scripts-Python/Teste01.py
@@ -0,0 +1,6 @@
+nome = input('Qual seu nome?')
+idade = input('Qual seu idade?')
+peso = input('Qual seu peso?')
+corfavorita = input('Qual sua cor favorita?')
+print(nome, idade, peso, corfavorita)
+
