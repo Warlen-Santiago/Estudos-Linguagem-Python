cf2708b1d7aed0c482aa1ce3a34cfd20e7fb0584
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-06-09 21:27:26 -0300

    Estudos dia 09/06/2025

---
+++ Curso Python - Curso em vídeo(Guanabara)/DesafiosPython/desafio02.py
@@ -0,0 +1,6 @@
+dia = input('Em qual dia voê nasceu?')
+mes = input('Em qual mês Você nasceu?')
+ano = input('Em qual ano você nasceu?')
+
+print('Você nasceu em ', dia, 'de', mes, 'de', ano)
+print('Seu aniversário é em', dia, 'de', mes, ', meus parabéns!')
