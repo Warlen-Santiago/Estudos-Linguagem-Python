0eba734429b6f5171a51cee61f0fc991a8c37717
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-06-13 21:05:12 -0300

    Friday Studies 13/06/2025

---
+++ Curso Python - Curso em vídeo(Guanabara)/Scripts-Python/aula07A.py
@@ -0,0 +1,16 @@
+n1 = int(input("Insira um número:" ))
+n2 = int(input("Insira outro número:" ))
+soma = n1 + n2
+sub = n1 - n2
+div = n1 / n2
+mul = n1 * n2
+divi = n1 // n2
+mod = n1 % n2
+pot = n1 ** n2
+print(f"A subtração entre eles é {n1-n2}" ) 
+print(f"A soma etre eles é {n1+n2}" )
+print(f"A multiplicação entre eles é {n1*n2}" )
+print(f"A divisão entre eles é {n1/n2}" )
+print(f"A divisão inteira entre eles é {n1//n2}" )
+print(f"O resto de divisão entre eles é {n1%n2}" )
+print(f"A potencia entre eles é \n{n1**n2}" )
