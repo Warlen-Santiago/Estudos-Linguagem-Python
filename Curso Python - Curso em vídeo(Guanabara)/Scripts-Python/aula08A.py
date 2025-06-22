8878dd2868ebd370c65e68f4b52fb22f4edcc4bf
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-06-22 11:19:06 -0300

    Sunday Studies 06/22/2025

---
+++ Curso Python - Curso em vídeo(Guanabara)/Scripts-Python/aula08A.py
@@ -0,0 +1,4 @@
+from math import sqrt, ceil
+num = int(input('Digite um número:\n '))
+raiz = sqrt(num)
+print(f'A raiz quadrada de {num} é {raiz} ')
---
+++ Curso Python - Curso em vídeo(Guanabara)/Scripts-Python/aula08C.py
@@ -0,0 +1,2 @@
+import emoji
+print(emoji.emojize('Olá, mundo :nerd_face: ', use_aliases=True))
