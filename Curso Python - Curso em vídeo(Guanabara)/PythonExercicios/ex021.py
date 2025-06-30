bf582afbea9b2a369c2b1d8acff98ff2a549b73a
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-06-29 22:24:33 -0300

    Update ex021.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex021.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex021.py
@@ -1,6 +1,13 @@
 import pygame
+import time
 
 pygame.init()
-pygame.mixer.music.load('keep.mp3')
+pygame.mixer.init()
+
+pygame.mixer.music.load('C:/Users/warle/Documents/MeusProjetos/Estudos-Linguagem-Python/Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/keep.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+
+while pygame.mixer.music.get_busy():
+        time.sleep(1)
+
+print("Reprodução finalizada.")
