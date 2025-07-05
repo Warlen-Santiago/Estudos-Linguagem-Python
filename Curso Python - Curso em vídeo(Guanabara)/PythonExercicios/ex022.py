6cfde1808b8bfde6f3367fa07c938fe0182be331
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-05 15:49:22 -0300

    Create ex022.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex022.py
@@ -0,0 +1,15 @@
+nome = input('Insira seu nome completo:\n ')
+
+print('Com todas as letras maiusculas:\n ')
+print(nome.upper())
+
+print('Com todas as letras minusculas:\n ')
+print(nome.lower())
+
+nomes = nome.split()
+print('Quantidade de letras do primeiro nome:\n ')
+print(len(nomes[0]))
+
+nomes_semspc = ''.join(nomes)
+print('Quantidade de letras do nome:\n ')
+print(len(nomes_semspc))
