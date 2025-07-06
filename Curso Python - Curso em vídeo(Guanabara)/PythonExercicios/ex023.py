86dfa044f17e7085dd9abdea38c9ae236e7888e6
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-06 19:48:07 -0300

    Create ex023.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex023.py
@@ -0,0 +1,14 @@
+num = str(input('Insira um número qualquer de 0 - 9999 para ver suas casas separadamente: '))
+
+numsep = list(num)
+
+num_qunt = len(num)
+
+if num_qunt == 4:
+    print(f'Casa das unidades: {numsep[0]}\nCasa das dezenas: {numsep[1]}\nCasa das centenas: {numsep[2]}\nCasa do unidade de milhar: {numsep[3]} ')
+elif num_qunt == 3:
+    print(f'Casa das unidades: {numsep[0]}\nCasa das dezenas: {numsep[1]}\nCasa das centenas: {numsep[2]} ')
+elif num_qunt == 2:
+    print(f'Casa das unidades: {numsep[0]}\nCasa das dezenas: {numsep[1]} ')
+else:
+    print(f'Casa das unidades: {numsep[0]}')
