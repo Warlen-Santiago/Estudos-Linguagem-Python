d7cd7d2f20b8ab8816280e3d9d4c0a6efa2ac78a
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-13 19:25:49 -0300

    Update ex025.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex025.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex025.py
@@ -1,9 +1,6 @@
 nome = input('Insira seu nome: ')
 
-v = nome.find('Silva')
-v2 = nome.find('silva')
-
-if 'silva' in nome or 'Silva' in nome:
+if 'silva' in nome.lower():
     print('Seu nome contem silva')
 else:
     print('Seu nome não contem silva')
