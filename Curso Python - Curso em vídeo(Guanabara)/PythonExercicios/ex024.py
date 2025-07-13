a57ea34f400dfb04c62bf8e41658601092d29435
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-13 19:11:13 -0300

    Update ex024.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex024.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex024.py
@@ -1,10 +1,8 @@
-cidade = input('Insira o nome da sua cidade para ver se ela começa com santo no nome: ')
+cidade = input('Insira o nome da sua cidade para ver se ela começa com santo no nome: ').strip()
 
 nome_separado = cidade.split()
 
-if nome_separado[0] == 'Santo':
-    print('Sim, sua cidade começa com santo no nome! ')
-elif nome_separado[0] == 'santo':
+if nome_separado[0].upper() == 'SANTO':
     print('Sim, sua cidade começa com santo no nome! ')
 else:
     print('não, sua cidade não começa com santo no nome. ')
