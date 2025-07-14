eb19732bb42aab91b2a3ec6bbd208bc7e09cb5f4
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-14 20:06:49 -0300

    Update ex027.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex027.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex027.py
@@ -2,7 +2,5 @@ nome = input('Insira seu nome completo: ')
 
 list_nome = nome.split()
 
-quant_list = len(list_nome)
-
 print(f'O primeiro nome é {list_nome[0]}. ')
-print(f'O ultimo nome é {list_nome[quant_list - 1]}. ')
+print(f'O ultimo nome é {list_nome[len(list_nome) - 1]}. ')
