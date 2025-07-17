6a6278d7b71c81f050605b4b539c5d2c7055b672
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-16 22:05:36 -0300

    Create aula10A.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/Scripts-Python/aula10A.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite a primeira nota '))
+n2 = float(input('Digite a segunda nota '))
+
+med = (n1 + n2)/2
+
+if med >=6:
+    print('Sua média foi boa Parabens! ')
+else:
+    print('Sua média foi baixa, pode melhorar ')
+    
