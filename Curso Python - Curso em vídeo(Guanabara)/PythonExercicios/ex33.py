a2409a9f69ce1f9ba396f40c58bcc70ccb446ee4
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-19 14:16:39 -0300

    Create ex33.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex33.py
@@ -0,0 +1,11 @@
+print('Insira três números: ')
+n1 = float(input('Primeiro número: '))
+n2 = float(input('Segundo número: '))
+n3 = float(input('Terceiro número: '))
+
+if n1 > n2 and n1 > n3:
+    print(f'{n1} é o maior número ')
+elif n2 > n1 and n2 > n3:
+    print(f'{n2} é o maior número ')
+elif n3 > n1 and n3 > n2:
+    print(f'{n3} é o maior número ')
