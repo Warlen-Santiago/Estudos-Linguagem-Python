a0e2e455df30b52fd67015c9e71ba7a528272177
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-22 19:17:43 -0300

    Update ex017.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex017.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex017.py
@@ -1,5 +1,5 @@
 from math import hypot
 print('Calculando a Hipotenusa de um triângulo retângulo: ')
-cat_op = float(input('Insira o cateto oposto:\n '))
-cat_ad = float(input('Insira o cateto adjacente:\n '))
+cat_op = float(input('Insira o \033[1;37mcateto oposto\033[m:\n '))
+cat_ad = float(input('Insira o \033[1;37mcateto adjacente\033[m:\n '))
 print(f'O valor da Hipotenusa desse triângulo é de {hypot(cat_op, cat_ad):.2f} ')
