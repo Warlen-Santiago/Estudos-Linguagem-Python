7068fb7c6541b0bb9e8f8464ae739f56da4c26ed
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-24 14:14:14 -0300

    Add exercise 38: compare two integers
    Implements a script that prompts the user for two integers and prints which one is greater or if they are equal.

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex38.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+
+if n1 > n2:
+    print(f'O número {n1} é maior ')
+elif n2 > n1:
+    print(f'O número {n2} é maior ')
+else:
+    print('Os números são iguais ')
