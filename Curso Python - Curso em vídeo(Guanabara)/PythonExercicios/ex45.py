7b3108845496b93673003cb79938cfbb91ec9738
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-07-28 21:03:18 -0300

    Create ex45.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex45.py
@@ -0,0 +1,12 @@
+from random import randint
+
+print('-joquempô- ')
+ej = int(input('Escolha:\n 1-pedra\n 2-papel\n 3-tesoura \n '))
+em = randint(1, 3)
+
+if ej == em:
+    print('Deu empate! ')
+elif ej == 1 and em == 2 or ej == 2 and em == 3 or ej == 3 and em == 1:
+    print('Você perdeu!!')
+elif ej == 2 and em == 1 or ej == 3 and em == 2 or ej == 1 and em ==3:
+    print('Você ganhou!!')
