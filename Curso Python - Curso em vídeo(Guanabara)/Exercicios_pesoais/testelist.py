a6eede8489a00f19d7ccc0785c76311ea277c744
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-08-21 20:02:19 -0300

    Update testelist.py

--- Curso Python - Curso em vídeo(Guanabara)/Exercicios_pesoais/testelist.py
+++ Curso Python - Curso em vídeo(Guanabara)/Exercicios_pesoais/testelist.py
@@ -6,3 +6,23 @@ item = input('Insira o item: ')
 lista.insert(pos-1, item)
 
 print(lista)
+
+'''Estudando listas = [], tuplas = () e dicionarios = {}'''
+print('-='*5,'Estudando listas = [], tuplas = () e dicionarios = {}','-='*5 )
+
+print(' ')
+
+print('As listas em python são uma otima maneira de armazenar grupos de dados. ')
+print('Dentro das listas podemos armazenar qualquer tipo de dado convencional ou mesclar tipos de dados, exemplo uma lista númerica: ')
+
+print(' ')
+'''Aqui nos criamos uma lista e ja a iniciamos com itens dentro da propria, para iniciar uma lista usamos variavel = [], assim teriamos uma lista vazia, essa em questão como ja dito inicializamos com números'''
+numeros = [1, 2, 3, 4, 5] 
+print(numeros)
+print(' ')
+
+e1 = input('Essa é uma lista númerica, mas se quiser pode adicionar um string em seu meio, digite seu nome para adicionar a lista: ')
+print(' ')
+
+numeros.insert(3, e1)
+print(numeros)
