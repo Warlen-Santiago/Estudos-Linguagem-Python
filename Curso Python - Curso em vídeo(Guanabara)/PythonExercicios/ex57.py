11cff42269e2ad2b6315e2ca6b7e97b6ed0d08a7
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-09-18 15:39:11 -0300

    Add exercise ex57 for gender input validation
    Implements a loop to prompt the user for gender input ('M' or 'F') until a valid option is provided, displaying a success message upon valid entry.

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex57.py
@@ -0,0 +1,6 @@
+sexo = ''
+while sexo not in ['M', 'F']:
+    sexo = input('Insira seu sexo[M/F]: ').upper()
+    if sexo not in ['M', 'F']:
+        print('Insira uma opção valida e tente novamente. ')
+print('Sexo inserido com sucesso. ')
