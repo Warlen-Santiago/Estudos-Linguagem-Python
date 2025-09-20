60af30fe8280f91ba9a42a03917a2eac49e29d2b
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-09-19 23:35:34 -0300

    Add guessing game exercise (ex58.py)
    Implements a number guessing game where the user tries to guess a randomly generated number between 1 and 10, tracking the number of attempts.

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex58.py
@@ -0,0 +1,20 @@
+from random import randint
+
+'''Gerando um número aleatorio'''
+num_aleatorio = randint(1, 10)
+'''iniciando o contador de tentativas'''
+contador_de_tentativas = 0
+'''iniciando a variavel que recebe o n do usuarío para comparação, necessario pois ele é presente no laço while, então precsa ser iniciado antes'''
+num = 0
+
+while num != num_aleatorio:
+    print('-'*10, f'{contador_de_tentativas+1}° TENTATIVA', '-'*10)
+    num = int(input('Um número de 1 a 10, qual você acha que é? '))
+    contador_de_tentativas += 1
+    if num != num_aleatorio:
+        print('Você errou, tente novamente! ')
+        print()
+
+print(f'O número é {num_aleatorio} parabéns você acertou! ')
+print(f'Tentou {contador_de_tentativas} vezes. ')
+
