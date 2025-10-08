91d9b679e969016c8918831616bb19ae85d7b377
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-10-08 16:40:31 -0300

    Add exercise ex69.py for person registration
    Implements a script to register people, tracking the number of individuals over 18, the number of men, and the number of women under 20. The script uses input validation and displays summary statistics at the end.

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex69.py
@@ -0,0 +1,39 @@
+print('-'*20)
+print('Cadastro de pessoas')
+print('-'*20)
+
+maisd18 = 0
+homens = 0
+mulhermenosd20 = 0
+
+
+
+while True:
+    sexo = ''
+    while sexo not in  ['M', 'F']:
+        sexo = input('Sexo [M/F]: ')[0].strip().upper()
+    idade = 0
+    while idade <= 0 or idade > 120:
+        idade = int(input('Idade: '))
+    escolha = ''
+    while escolha not in  ['S', 'N']:
+        escolha = input('Quer continuar [S/N]: ')[0].strip().upper()
+
+    if idade > 18:
+        maisd18+=1
+    if sexo == 'M':
+        homens+=1
+    if sexo == 'F' and idade < 20:
+        mulhermenosd20+=1
+    
+    if escolha == 'N':
+        break
+
+print('-'*20)
+
+print(f'Foram cadastradas {maisd18} pessoas com mais de 18 anos. ')
+print()
+print(f'Foram cadastrados {homens} homens. ')
+print()
+print(f'Foram cadastradas {mulhermenosd20} mulheres com menos de 20 anos. ')
+
