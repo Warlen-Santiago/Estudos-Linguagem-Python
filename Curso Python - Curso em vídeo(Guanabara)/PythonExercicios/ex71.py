9a67472a05f780bc4a301c33e26acbfcd48dfca4
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-10-10 11:02:34 -0300

    Add ATM cash withdrawal simulation script (ex71.py)
    Implements a script that calculates the minimum number of banknotes (R$50, R$20, R$10, R$1) needed for a user-specified withdrawal amount. The script prompts the user for an amount and displays the breakdown of notes to be dispensed.

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex71.py
@@ -0,0 +1,30 @@
+print('='*40)
+print('-'*15,'BANCO AC','-'*15)
+print('='*40)
+
+'''Recendo valor que o usuario deseja sacar'''
+valor = int(input('Insira o valor que deseja sacar: '))
+
+'''uma listagem com as notas disponiveis sendo indexadas da seguinte forma: [0] = representa notas de 50, [1] = de 20, [2] = de 10, [3] = de 1 '''
+notas_disponiveis = [50, 20, 10, 1]
+'''lista para armazenar quantas notas vão ser retiradas de cada, iniciada em zero pois o calculo ainda não foi feito'''
+n = [0, 0, 0, 0]
+
+'''contador é essencial, sera dividido em 4 etapas pois são quatro variações de cedulas disponiveis '''
+cont=0
+while True:
+    '''começamos sabendo quantas cedulas serão do maior valor como indexado nas listagens acima'''
+    n[cont] = valor // notas_disponiveis[cont]
+    '''depois atualizamos o valor para que receba o valor descontado das cedulas que ja foram contadas, assim quando o ciclo se repitir o valor estara atualizado conforme a necessidade'''
+    valor -= notas_disponiveis[cont] * n[cont]
+
+    cont+=1
+    if cont == 4:
+        break
+print('='*40)
+
+print('Serão liberadas as seguintes cedulas:')
+print(f'''R$ 50: {n[0]}
+R$ 20: {n[1]}
+R$ 10: {n[2]}
+R$ 1: {n[3]}''')
