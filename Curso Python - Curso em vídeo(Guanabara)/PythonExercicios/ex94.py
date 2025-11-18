630b0b72a35943d4ba9ac3643755e93df82c5954
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-11-17 23:12:33 -0300

    Add script for person registration and analysis
    Introduces ex94.py, a script that registers people with name, sex, and age, calculates the average age, lists registered women, and identifies people above the average age.

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex94.py
@@ -0,0 +1,54 @@
+print(f'{'CADASTRO DE PESSOAS':^50} ')
+print('-'*50)
+
+pessoas = []
+while True:
+    p = {
+        'Nome' : input('Nome: ').capitalize(),
+        'Sexo' : input('Sexo:[M/F]')[0].upper().strip(),
+        'Idade' : int(input('Idade: '))
+    }
+    pessoas.append(p)
+    escolha = ''
+    while escolha not in ('N', 'S'):
+        escolha = input('Deseja cntinuar:[S/N] ')[0].upper().strip()
+    print()
+    if escolha == 'N':
+        break
+
+print('-'*50)
+print(f'{'DADOS CADASTRADOS':^50} ')
+print('-'*50)
+
+print(f'Foram cadastradas {len(pessoas)} pessoas. ')
+print()
+
+s_idade = 0
+
+for c in pessoas:
+    for k,v in c.items():
+        if k == 'Idade':
+            s_idade+=v
+
+med_idade = s_idade/len(pessoas)
+
+print(f'A média de idade do grupo é {med_idade:.2f}. ')
+print('-'*50)
+
+print('As mulheres cadastradas foram:')
+for c in pessoas:
+    for k,v in c.items():
+        if k == 'Sexo' and v == 'F':
+            print(f'-{c['Nome']}')
+print()
+
+print('As pessoas com idade acima da média são: ')
+for c in pessoas:
+    for k,v in c.items():
+        if k == 'Idade' and v > med_idade:
+            print(f'-{c['Nome']} com {c['Idade']} anos.')
+
+print()
+print('-'*50)
+print()
+           
