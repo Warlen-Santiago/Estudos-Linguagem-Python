bc4d65be5bb9a8f65f67c5c20b679e6ecb059e99
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-12-01 22:59:42 -0300

    Create ex99.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonExercicios/ex99.py
@@ -0,0 +1,36 @@
+def maior(n):
+    m = max(n)
+    print(f'E o maior número da lista é: {m}')
+
+def linha():
+    print('-'*50)
+
+
+linha()
+print(f'{'LISTA DE NÚMEROS':^50}')
+linha()
+
+
+numeros = []
+cont=1
+while True:
+    numero = int(input(f'Insira o {cont}° número: '))
+    numeros.append(numero)
+    cont+=1
+
+    escolha =''
+    while escolha not in ('N','S'):
+        escolha = input('Deseja continuar?[S/N] ')[0].upper().strip()
+    linha()
+    if escolha == 'N':
+        break
+    
+
+print('Sua lista de números ficou assim: ')
+for c in numeros:
+    print(c,end='-')
+print('FIM.')
+print()
+
+maior(numeros)
+print()
