fc38024347fcd0d1ca5058bbf48842c29fec0828
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2025-12-05 16:53:22 -0300

    A function that validates whether the input data is an integer.

---
+++ Funções/validaint.py
@@ -0,0 +1,10 @@
+def leiaint(n):
+    
+    num = input(n)
+    while num.isnumeric() == False: 
+        print('\n\033[31mErro... O valor inserido é inavlido, tente novamente.\033[m\n ')
+        num = input(n)
+
+    return num
+
+n = leiaint('Digite um numero inteiro: ')
