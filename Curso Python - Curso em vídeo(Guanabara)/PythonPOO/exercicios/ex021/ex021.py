87dd04a2a84316739a60a4ebf6ddb31b7127118d
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2026-03-06 15:32:26 -0300

    Create ex021.py

---
+++ Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex021/ex021.py
@@ -0,0 +1,54 @@
+from rich import print
+
+
+class Caneta():
+    def __init__(self, cor='Branco'):
+        self.cor = cor
+        self.tampa = 'tampada'
+
+        if self.cor.lower() == 'branco':
+            self.cor = 'white'
+
+        elif self.cor.lower() == 'vermelho':
+            self.cor = 'red'
+
+        elif self.cor.lower() == 'verde':
+            self.cor = 'green'
+
+        elif self.cor.lower() == 'azul':
+            self.cor = 'blue'
+
+        elif self.cor.lower() == 'roxo':
+            self.cor = 'purple'
+
+        elif self.cor.lower() == 'laranja':
+            self.cor = 'orange'
+
+        elif self.cor.lower() == 'amarelo':
+            self.cor = 'yellow'
+
+        elif self.cor.lower() == 'preto':
+            self.cor = 'black'
+
+
+    def destampar(self):
+        self.tampa = 'destampado'
+
+    def quebra_linha(self,q=1):
+        for c in range(0,q):
+            print('')
+
+    def escrever(self,txt):
+        if self.tampa == 'destampado':
+            print(f'[{self.cor}]{txt}[/]')
+        else:
+            print('🚫[red] Sua caneta está[/][purple] tampada[/],[red] a [/][purple]destampe[/] [red] para escrever![/]🖊️✍️ ')
+
+
+caneta1 = Caneta('vermelho')
+caneta1.destampar()
+caneta1.escrever('Mixerica')
+caneta1.quebra_linha(3)
+caneta1.escrever('Mixerica')
+caneta1.quebra_linha()
+        
