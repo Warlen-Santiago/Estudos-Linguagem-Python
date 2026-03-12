9f943af46e8999ac0bae0a62c8ff01a9edb12fb9
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2026-03-11 21:28:47 -0300

    Update ex016.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex016/ex016.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex016/ex016.py
@@ -3,16 +3,17 @@ from rich.panel import Panel
 
 
 class Funcionario:
-
-    # DEFININDO ATRIBUTOS
+    #ATRIBUTO DE CLASSE
+    empresa = 'Escritorio araujo e mendes'
+    # DEFININDO ATRIBUTOS DE INSTANCIA DO OBJETO
     def __init__(self, nome='Não informado', setor='Não informado', cargo='Não informado'):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
     # DEFININDO METODOS
-    def apresentação(self, empresa='Escritório Araujo e Mendes'):
-        painel = Panel((f'Olá, Muito prazer!😁 Me chamo [purple]{self.nome}[/], sou [purple]{self.cargo}[/] e atuo no stor de [purple]{self.setor} na empresa {empresa}[/]'))
+    def apresentação(self):
+        painel = Panel((f'Olá, Muito prazer!😁 Me chamo [purple]{self.nome}[/], sou [purple]{self.cargo}[/] e atuo no stor de [purple]{self.setor} na empresa {Funcionario.empresa}[/]'))
 
         return painel
 
