aa3d736ff76d3653acf77a35e685f471c7e5f9d5
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2026-03-14 12:53:44 -0300

    Update ex018.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex018/ex018.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex018/ex018.py
@@ -3,14 +3,19 @@ from rich.panel import Panel
 
 
 class Churas():
+    #ATRIBUTOS DA CLASSE
+    valorKg=35.70
+    quant_consum=0.600
+
+    #ATRIBUTOS DE INSTANCIA DO OBJETO
     def __init__(self, titulo='Churasco', pessoas=1):
         self.quant = pessoas
         self.titulo = titulo
 
-    def analise(self, valorKg=35.70, quant_consum=0.600):
+    def analise(self):
 
-        quant_carne = self.quant * quant_consum
-        total_custo = quant_carne * valorKg
+        quant_carne = self.quant * Churas.quant_consum
+        total_custo = quant_carne * Churas.valorKg
         valor_pessoa = total_custo/self.quant
 
         resultado = Panel(f'O churasco para {self.quant} pessoas tem estimativa de {quant_carne:.3f} Kg de carne no total\n o custo total do churasco saira em R$ {total_custo:.2f} e o individual R$ {valor_pessoa:.2f}', title=self.titulo, expand=False)
