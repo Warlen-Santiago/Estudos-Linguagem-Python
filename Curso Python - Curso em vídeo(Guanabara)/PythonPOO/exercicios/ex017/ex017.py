5a5388d01b45c941e92945c33806f1b80cff954f
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2026-03-14 12:53:36 -0300

    Update ex017.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex017/ex017.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonPOO/exercicios/ex017/ex017.py
@@ -7,9 +7,12 @@ class Produto():
         self.nome = nome
         self.preço = preço
 
+    def __str__(self):
+        return f'O profuto {self.nome} custa {self.preço}'
+
     def etiqueta(self):
-        q = len(self.nome) 
-        etq = Panel(f'{self.nome}\n{'-'*q}\n[green]R$ {self.preço:.2f}[/]', expand=False)
+        preço = f'R$ {self.preço:.2f}'
+        etq = Panel(f'{self.nome.center(30, ' ')}\n{'-'*30}\n[green]{preço.center(30,' ')}[/]', expand=False)
         return etq
     
 p1 = Produto('Garrafa de Água', 32.50)
