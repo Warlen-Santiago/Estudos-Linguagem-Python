2ad389177a8ab910615178c37113d6c2f12bf2f5
WARLEN SANTIAGO PAIXAO OLIVEIRA <warlen2004@gmail.com>
2026-04-06 21:59:33 -0300

    Update alimento.py

--- Curso Python - Curso em vídeo(Guanabara)/PythonPOO/Exercicios_pessoais/ex_herança/lib/alimento.py
+++ Curso Python - Curso em vídeo(Guanabara)/PythonPOO/Exercicios_pessoais/ex_herança/lib/alimento.py
@@ -1,9 +1,22 @@
 from .produto import Produto
 from rich import print
 from rich.panel import Panel
+from datetime import date
 
 
 class Alimento(Produto):
-    def __init__(self, nome, marca, tipo, lote, validade, entrada, valor=0, quant=0):
+    def __init__(self, nome, marca, tipo, lote, validade=(0000,00,00), valor=0, quant=0):
         super().__init__(nome, marca, tipo, valor, quant)
+        self.lote = lote
+        self.validade = date(*validade)
+         
+    def vencimento(self):
+        data_atual = date.today()
+
+        if data_atual < self.validade:
+            exibir = Panel(f'O produto {self.nome} da marca {self.marca} [green]vencera em {self.validade}[/] ', expand=False, title=self.lote)
+            return exibir
         
+        else:
+            exibir = Panel(f'O produto {self.nome} da marca {self.marca} [red]venceu em {self.validade}[/] ', expand=False, title=self.lote)
+            return exibir
